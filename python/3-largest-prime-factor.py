00afaf0cd2ef08cbe08c2d5cd4e78db72e42e824
Rai Icasiano <ricasiano@gmail.com>
2019-06-11 20:17:05 +0800

    fix bug when identifying a prime number

--- python/3-largest-prime-factor.py
+++ python/3-largest-prime-factor.py
@@ -1,13 +1,13 @@
 import math
 
-given_number = 600851475143
-
 
 def is_a_factor_of_number(dividend: float, divisor: float):
     return False if 0 != dividend % divisor else True
 
 
 def is_a_prime_number(number: float):
+    if math.sqrt(number).is_integer():
+        return False
     for counter in range(int(number - 1), 2):
         if number % counter == 0:
             return False
@@ -17,8 +17,10 @@ def is_a_prime_number(number: float):
 def largest_prime_factor(number: float):
     for counter in range(2, int(math.ceil(number / 2))):
         if is_a_factor_of_number(number, counter):
-            return round(number / counter)
+            largest_factor = round(number / counter)
+            if is_a_prime_number(largest_factor):
+                return largest_factor
     return 1
 
 
-print(largest_prime_factor(given_number))
+print(largest_prime_factor(600851475143))

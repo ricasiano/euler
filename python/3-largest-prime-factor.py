8507a8a368e87c8f07d52d8d587ff7772776136d
Rai Icasiano <ricasiano@gmail.com>
2019-06-11 19:56:48 +0800

    refactor to remove var assignments

--- python/3-largest-prime-factor.py
+++ python/3-largest-prime-factor.py
@@ -8,21 +8,16 @@ def is_a_factor_of_number(dividend: float, divisor: float):
 
 
 def is_a_prime_number(number: float):
-    if 0 == number % 2 and math.sqrt(number).is_integer():
-        return False
-    counter = number - 1
-    while counter > 2:
+    for counter in range(int(number - 1), 2):
         if number % counter == 0:
             return False
-        counter = counter - 1
     return True
 
 
-def largest_prime_factor(number, counter=2):
-    while counter < math.ceil(number / 2):
+def largest_prime_factor(number: float):
+    for counter in range(2, int(math.ceil(number / 2))):
         if is_a_factor_of_number(number, counter):
             return round(number / counter)
-        counter = counter + 1
     return 1
 
 
